f4d7390a40bc6655be0832eb3ce2062bbaec927b
Evgeniy.Skiba <Skiba.Evgeniy@hotmail.com>
2018-04-03 13:54:21 +0300

    added readme -- nested file

---
+++ README.txt
@@ -0,0 +1,6 @@
+http://take.ms/3OYU8
+
+ But you have to search for the x86 version and the .NET framework version you are targetting in your project. Last you need to download the bundle version!
+ 
+ sqlite-netFx451-setup-bundle-x86-2013-1.0.108.0.exe 
+(10.51 MiB)	
---
+++ SqLiteCodeFirstSample.DataBase/context/work.cs
@@ -0,0 +1,36 @@
+﻿using SQLite.CodeFirst;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqLiteCodeFirstSample.DataBase.context
+{
+
+
+    [System.ComponentModel.DataAnnotations.Schema.Table("work")]
+    public partial class work
+    {
+
+        [Autoincrement]
+        [Key]
+        public long id { get; set; }
+
+        [Required]
+        [StringLength(2147483647)]
+        public string name { get; set; }
+
+
+        public virtual ICollection<customer> customers { get; set; }
+
+
+        public work()
+        {
+            customers = new HashSet<customer>();
+        }
+    }
+}
